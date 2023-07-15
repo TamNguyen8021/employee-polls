a38bae15a80cecc2633b4a3cb37d19215ce73ed1
TamNguyen8021 <tamntt8021@gmail.com>
2023-07-15 16:44:25 +0700

    feat: Add page navigation for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,34 @@
-import { HOME, LEADERBOARD, NEW, LOGOUT } from "constants";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import {
+	HOME,
+	LEADERBOARD,
+	LOGOUT,
+	NEW,
+	HOME_PATH,
+	LEADERBOARD_PATH,
+	LOGIN_PATH,
+	NEW_PATH,
+} from "constants";
 import classNames from "classnames";
 import { cssClasses } from "cssClasses";
 
 /**
  * @description Represents the navigation header
  */
-const Header = (props) => {
-	const headers = [HOME, LEADERBOARD, NEW];
+const Header = ({ onClick }) => {
+	const navigate = useNavigate();
+	const headers = [
+		{ name: HOME, path: HOME_PATH },
+		{ name: LEADERBOARD, path: LEADERBOARD_PATH },
+		{ name: NEW, path: NEW_PATH },
+	];
 	const [selectedHeader, setSelectedHeader] = useState(HOME);
 
 	const handleSelectHeader = (header) => {
 		setSelectedHeader(header);
+		navigate(header.path);
 	};
 
 	return (
@@ -20,24 +37,31 @@ const Header = (props) => {
 				<ul className={classNames([cssClasses.flex, "list-menu"])}>
 					{headers.map((header) => (
 						<li
-							key={header}
+							key={header.name}
 							className={classNames([
 								"menu-header",
 								{ selected: selectedHeader === header },
 							])}
 							onClick={() => handleSelectHeader(header)}>
-							{header}
+							{header.name}
 						</li>
 					))}
 				</ul>
 			</nav>
-			<input
-				className="btn-logout"
-				type="button"
-				value={LOGOUT}
-			/>
+			<Link to={LOGIN_PATH}>
+				<input
+					className="btn-logout"
+					type="button"
+					value={LOGOUT}
+					onClick={onClick}
+				/>
+			</Link>
 		</div>
 	);
+};
+
+Header.propTypes = {
+	onClick: PropTypes.func.isRequired,
 };
 
 export default Header;
