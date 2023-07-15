b6573947d63bd26f198d6207bf6ba0a0e3e3c9f8
TamNguyen8021 <tamntt8021@gmail.com>
2023-07-15 21:16:44 +0700

    refactor: Move home page render logic to HomePage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Header from "components/Header";
 import HomePage from "components/pages/HomePage";
 import LeaderboardPage from "components/pages/LeaderboardPage";
 import NewPage from "components/pages/NewPage";
 import PollDetails from "components/poll/PollDetails";
-import PollSection from "components/poll/PollSection";
 import { logout } from "reducers/userSlice";
 import {
 	HOME_PATH,
@@ -15,49 +14,15 @@ import {
 	POLL_PATH,
 } from "constants";
 import LoginPage from "components/pages/LoginPage";
-import { fetchPolls, selectPolls } from "reducers/pollSlice";
-import { useEffect } from "react";
 
 const App = () => {
 	const location = useLocation();
 	const navigate = useNavigate();
-	const polls = useSelector(selectPolls);
 	const dispatch = useDispatch();
-
-	useEffect(() => {
-		dispatch(fetchPolls());
-	}, []);
 
 	const handleLogout = () => {
 		dispatch(logout());
 		navigate(LOGIN_PATH);
-	};
-
-	const filterAnsweredQuestions = () => {
-		const answeredQuestions = [];
-
-		for (const value of Object.values(polls)) {
-			if (
-				value.optionOne.votes.includes(location.state?.id) ||
-				value.optionTwo.votes.includes(location.state?.id)
-			) {
-				answeredQuestions.push(value);
-			}
-		}
-
-		return answeredQuestions;
-	};
-
-	const filterUnansweredQuestions = () => {
-		const unanweredQuestions = [];
-		const answeredQuestions = filterAnsweredQuestions();
-
-		for (const [key, value] of Object.entries(polls)) {
-			if (!answeredQuestions.find((question) => question.id === key)) {
-				unanweredQuestions.push(value);
-			}
-		}
-		return unanweredQuestions;
 	};
 
 	return (
@@ -71,18 +36,7 @@ const App = () => {
 				/>
 				<Route
 					path={HOME_PATH}
-					element={
-						<HomePage>
-							<PollSection
-								title="New Questions"
-								polls={filterUnansweredQuestions()}
-							/>
-							<PollSection
-								title="Done"
-								polls={filterAnsweredQuestions()}
-							/>
-						</HomePage>
-					}
+					element={<HomePage />}
 				/>
 				<Route
 					path={NEW_PATH}
--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,8 +1,60 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { fetchPolls, selectPolls } from "reducers/pollSlice";
+import PollSection from "components/poll/PollSection";
+
 /**
  * @description Represents the home page
  */
-const HomePage = (props) => {
-	return <div className="home-page">{props.children}</div>;
+const HomePage = () => {
+	const location = useLocation();
+	const dispatch = useDispatch();
+	const polls = useSelector(selectPolls);
+
+	useEffect(() => {
+		dispatch(fetchPolls());
+	}, []);
+
+	const filterAnsweredQuestions = () => {
+		const answeredQuestions = [];
+
+		for (const value of Object.values(polls)) {
+			if (
+				value.optionOne.votes.includes(location.state?.id) ||
+				value.optionTwo.votes.includes(location.state?.id)
+			) {
+				answeredQuestions.push(value);
+			}
+		}
+
+		return answeredQuestions;
+	};
+
+	const filterUnansweredQuestions = () => {
+		const unanweredQuestions = [];
+		const answeredQuestions = filterAnsweredQuestions();
+
+		for (const [key, value] of Object.entries(polls)) {
+			if (!answeredQuestions.find((question) => question.id === key)) {
+				unanweredQuestions.push(value);
+			}
+		}
+		return unanweredQuestions;
+	};
+
+	return (
+		<div className="home-page">
+			<PollSection
+				title="New Questions"
+				polls={filterUnansweredQuestions()}
+			/>
+			<PollSection
+				title="Done"
+				polls={filterAnsweredQuestions()}
+			/>
+		</div>
+	);
 };
 
 export default HomePage;
