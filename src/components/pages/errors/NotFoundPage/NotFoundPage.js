9122eeab5399bd7b270177bc0f3c6409a12d5de7
TamNguyen8021 <tamntt8021@gmail.com>
2023-07-19 19:59:14 +0700

    fix: Show header on not found page

--- src/components/pages/errors/NotFoundPage/NotFoundPage.js
+++ src/components/pages/errors/NotFoundPage/NotFoundPage.js
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { LOGIN_PATH, NOT_FOUND_PATH } from "constants";
+
 const NotFoundPage = () => {
+	const location = useLocation();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!location.state?.id) {
+			navigate(LOGIN_PATH, { state: { from: NOT_FOUND_PATH } });
+		}
+	}, []);
+
 	return (
 		<div>
-			<p>Page Not Found. Did you enter correct url?</p>
+			<h1>Page Not Found</h1>
+			<p>Did you enter correct url?</p>
 		</div>
 	);
 };
